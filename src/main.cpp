f1354c57cc3d0ac9addf51c4ebc7f1540499f10f
Tristan Seifert <tristan@tseifert.me>
2020-05-10 13:50:15 -0700

    Backspace machine goes brrrrrrrrrrr
    fr tho we gonna rewrite this thing

--- src/main.cpp
+++ src/main.cpp
@@ -8,38 +8,19 @@
 
 #include <pthread.h>
 
-#include "INIReader.h"
+#include <libconfig.h++>
 
 #include <iostream>
 #include <atomic>
 
 #include <signal.h>
 
-#include "CommandServer.h"
-#include "ProtocolHandler.h"
-#include "DataStore.h"
-#include "EffectRunner.h"
-#include "Routine.h"
-
 // when set to false, the server terminates
 std::atomic_bool keepRunning;
-
-// data store
-static DataStore *store = nullptr;
-
-// various components of the server
-static CommandServer *cs = nullptr;
-static ProtocolHandler *protocol = nullptr;
-static EffectRunner *runner = nullptr;
 
 // define flags
 DEFINE_string(config_path, "./lichtenstein.conf", "Path to the server configuration file");
 DEFINE_int32(verbosity, 4, "Debug logging verbosity");
-
-// parsing of the config file
-INIReader *configReader = nullptr;
-
-void parseConfigFile(const std::string &path);
 
 /**
  * Signal handler. This handler is invoked for the following signals to enable
@@ -63,13 +44,10 @@ int main(int argc, char *argv[]) {
 	google::InitGoogleLogging(argv[0]);
 	google::InstallFailureSignalHandler();
 
-	LOG(INFO) << "lichtenstein server " << gVERSION_HASH << "/" << gVERSION_BRANCH;
+	LOG(INFO) << "lichtenstein server " << gVERSION_HASH << "/" << gVERSION_BRANCH << " starting up";
 
-	// interpret command-line flags
+	// interpret command-line flags and read config
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-	// first, parse the config file
-	parseConfigFile(FLAGS_config_path);
 
 	// set thread name
 	#ifdef __APPLE__
@@ -91,117 +69,8 @@ int main(int argc, char *argv[]) {
 
 	sigaction(SIGINT, &sigIntHandler, nullptr);
 
-	// load the datastore from disk
-	store = new DataStore(configReader);
-
-	// start the protocol parser (binary lichtenstein protocol)
-	protocol = new ProtocolHandler(store, configReader);
-
-	// start the effect evaluator
-	runner = new EffectRunner(store, configReader, protocol);
-
-	// start the external command interpreter (JSON socket)
-	cs = new CommandServer(store, configReader, runner);
-	cs->start();
-
-	// XXX: Test the routine code
-	/*std::vector<DbRoutine *> routines = store->getAllRoutines();
-	DataStore::Routine *r = routines[0];
-	LOG(INFO) << "Found routines: " << *r;
-
-	Routine *rout;
-
-	try {
-		rout = new Routine(r);
-	} catch (Routine::LoadError e) {
-		LOG(ERROR) << "Error loading script: " << e.what();
-	}
-
-	std::vector<HSIPixel> buf;
-	buf.resize(300, {0, 0, 0});
-	rout->attachBuffer(&buf);
-
-	for(int i = 0; i < 1000; i++) {
-		rout->execute(i);
-	}
-
-	LOG(INFO) << "Average script execution time: " << rout->getAvgExecutionTime() << "µS";
-	*/
-
-	// XXX: Test the effect evaluation code
-	std::vector<DbGroup *> groups = store->getAllGroups();
-	std::vector<DbRoutine *> routines = store->getAllRoutines();
-
-	auto mapper = runner->getMapper();
-
-	for(int i = 0; i < groups.size(); i++) {
-		DbGroup *dbG = groups[i];
-
-		unsigned int routineIndex = i;
-
-		if(routineIndex >= routines.size()) {
-			routineIndex = (routines.size() - 1);
-		}
-		DbRoutine *dbR = routines[routineIndex];
-
-		// create the output group and add the mapping
-		OutputMapper::OutputGroup *g = new OutputMapper::OutputGroup(dbG);
-		Routine *r = new Routine(dbR);
-
-		mapper->addMapping(g, r);
-	}
-
 	// wait for a signal
 	while(keepRunning) {
 		pause();
 	}
-
-	// stop the servers
-	cs->stop();
-
-	// clean up
-	delete cs;
-	delete runner;
-	delete protocol;
-
-	// delete the datastore last
-	delete store;
-}
-
-/**
- * Opens the config file for reading and parses it.
- */
-void parseConfigFile(const std::string &path) {
-	int err;
-
-	LOG(INFO) << "Reading configuration from " << path;
-
-	// attempt to open the config file
-	configReader = new INIReader(path);
-
-	err = configReader->ParseError();
-
-	if(err == -1) {
-		LOG(FATAL) << "Couldn't open config file at " << path;
-	} else if(err > 0) {
-		LOG(FATAL) << "Parse error on line " << err << " of config file " << path;
-	}
-
-	// set up the logging parameters
-	int verbosity = configReader->GetInteger("logging", "verbosity", 0);
-
-	if(verbosity < 0) {
-		FLAGS_v = abs(verbosity);
-		FLAGS_minloglevel = 0;
-
-		LOG(INFO) << "Enabled verbose logging up to level " << abs(verbosity);
-	} else {
-		// disable verbose logging
-		FLAGS_v = 0;
-
-		// ALWAYS log FATAL errors
-		FLAGS_minloglevel = std::min(verbosity, 2);
-	}
-
-	FLAGS_logtostderr = configReader->GetBoolean("logging", "stderr", true);
 }
