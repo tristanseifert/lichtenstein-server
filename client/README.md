ea5a5cbf8786d77e68102bd81924f3ca0aa8f547
Tristan Seifert <tristan@tseifert.me>
2020-05-25 17:00:46 -0700

    Create base for client code
    - Basic skeleton of client code created; it includes the shared code for
      logging and config reading.

---
+++ client/README.md
@@ -0,0 +1,18 @@
+# Lichtenstein Client
+A relatively basic C++ implementation of a Lichtenstein LED client. It connects to a server (see `server` directory,) authenticates and can then receive pixel data. Internally, one or more output plugins are set up (or, in some cases, automagically detected) in the configuration which actually send this data to physical LEDs.
+
+## Building
+Once all dependencies have been satisfied (remember to pull submodules) you can invoke CMake to generate projects for your favorite build system. 
+
+You will need [LibreSSL](https://www.libressl.org/) and the [Cap'n Proto](https://capnproto.org/) runtime for network communication. These are the only system-wide dependencies required; all others are provided as submodules.
+
+As an example, you might do the following to build:
+```
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
+make
+```
+
+### macOS
+Install libressl and capnp via Homebrew; then invoke CMake. Everything should compile without problems.
---
+++ client/src/main.cpp
@@ -0,0 +1,184 @@
+/**
+ * Main entrypoint for Lichtenstein client
+ */
+#include "version.h"
+
+#include <atomic>
+#include <iostream>
+#include <sstream>
+
+#include <unistd.h>
+#include <signal.h>
+
+#include <cxxopts.hpp>
+
+#include "ConfigManager.h"
+#include "Logging.h"
+
+// bring all our namespaces into scope
+// using namespace Lichtenstein::Client;
+
+// when set to false, the server terminates
+static std::atomic_bool keepRunning;
+
+// command line configuration
+static struct {
+    // config file path
+    std::string cfgPath = "./lichtenstein.conf";
+} cmdline;
+
+/**
+ * Signal handler. This handler is invoked for the following signals to enable
+ * us to do a clean shut-down:
+ *
+ * - SIGINT
+ */
+static void signalHandler(int sig) {
+    Logging::warn("Caught signal {}; shutting down!", sig);
+    keepRunning = false;
+
+    // mask the signal now
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, sig);
+
+    sigprocmask(SIG_BLOCK, &set, nullptr);
+}
+
+/**
+ * Parses the command line. If false is returned, the program should not
+ * continue further.
+ */
+static bool ParseCmdLine(int argc, char *argv[]) {
+    using namespace cxxopts;
+
+    Options opt("lichtenstein_client", "Lichtenstein effects client");
+
+    opt.add_options()
+        ("c,config", "Path to config file")
+        ("version", "Print program version and exit")
+        ("h,help", "Print usage and exit")
+    ;
+
+    // parse the options
+    try {
+        auto result = opt.parse(argc, argv);
+
+        if(result.count("config")) {
+            cmdline.cfgPath = result["config"].as<std::string>();
+        }
+
+        // print help?
+        if(result.count("help")) {
+            std::cout << opt.help() << std::endl;
+            return false;
+        }
+        // print version?
+        else if(result.count("version")) {
+            std::cout << "lichtenstein_client " << gVERSION << " (" 
+                << std::string(gVERSION_HASH).substr(0, 8) << ")" << std::endl;
+            return false;
+        }
+    } catch(option_not_exists_exception &e) {
+        std::cerr << "Failed to parse command line: " << e.what() << std::endl;
+        return false;
+    }
+
+    // we good
+    return true;
+}
+
+/**
+ * Reads the config file and configures early settings. Returns false if an
+ * error took place, true otherwise.
+ */
+static bool LoadConfig(void) {
+    // try to read the config file
+    try {
+        ConfigManager::readConfig(cmdline.cfgPath);
+    } catch(ConfigManager::ParseException &e) { 
+        std::cerr << "Parse error on line " << e.getLine() << " of config: "
+            << e.what() << std::endl;
+        return false;
+    } catch(std::exception &e) {
+        std::cerr << "Failed to read config from '" << cmdline.cfgPath
+            << "' (" << e.what() << ")" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Starts all required services. Any exceptions during this process will result
+ * in an immediate termination.
+ */
+void StartServices() {
+    try {
+        // TODO: :)
+    } catch(std::exception &e) {
+        Logging::crit("StartServices() failed: {}", e.what());
+        Logging::stop();
+
+        std::abort();
+    }
+}
+
+/**
+ * Stops all services that were started earlier.
+ */
+void StopServices() {
+    try {
+        // TODO: :)
+    } catch(std::exception &e) {
+        Logging::crit("StopServices() failed: {}", e.what());
+        Logging::stop();
+
+        std::abort();
+    }
+
+    Logging::info("All services stopped");
+}
+
+
+
+/**
+ * Main function
+ */
+int main(int argc, char *argv[]) {
+    // parse command line, load config and set up logging
+    if(!ParseCmdLine(argc, argv)) {
+        return 0;
+    }
+    if(!LoadConfig()) {
+        return -1;
+    }
+
+    Logging::start();
+
+    // set up a signal handler for termination so we can close down cleanly
+    keepRunning = true;
+
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = signalHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, nullptr);
+
+    // start all required services
+    StartServices();
+
+    // wait for a signal
+    Logging::info("lichtenstein_client is ready");
+
+    while(keepRunning) {
+        ::pause();
+    }
+
+    // shut down all allocated services
+    StopServices();
+    Logging::stop();
+}
+
